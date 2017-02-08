e9d0d369fbcb55bc22d4d66dfeff33794c08c7a3
Kalle Heinonen <kalle.m.heinonen@gmail.com>
2017-02-09 01:11:15 +0200

    Added DevModeCheck. The game can now be played from any scene, and it just works.

---
+++ Assets/Scripts/DevModeCheck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TamkRunner
+{
+	public class DevModeCheck : MonoBehaviour {
+
+		public string CurrentSceneName;
+
+		// Use this for initialization
+		void Awake () {
+			GameObject persistentObjects = GameObject.Find ("PersistentObjects");
+
+			if (persistentObjects == null)
+			{
+				Debug.Log ("Persistent Objects gameobject not found. Entering DMC.");
+				DontDestroyOnLoad (gameObject);
+				SceneManager.LoadScene ("SplashScreen");
+			}
+		}
+	}
+}
--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -7,19 +7,46 @@ namespace TamkRunner
 {
     public class GameStateManager : MonoBehaviour
     {
+		private string _devModeSceneToLoad = "";
+
+		public void Awake()
+		{
+			DontDestroyOnLoad(GameObject.Find("PersistentObjects"));
+			// Check for Dev Mode
+			GameObject obj = GameObject.Find("DevModeCheck");
+			if (obj != null) {
+				Debug.Log ("Found DMC, entering dev mode...");
+				DevModeCheck scene = obj.GetComponent<DevModeCheck> ();
+				_devModeSceneToLoad = scene.CurrentSceneName;
+				Destroy (obj);
+			}
+		}
+
+		void Start ()
+		{
+			// If DMC was activated, skip straight to Level1.
+			if (_devModeSceneToLoad != "")
+			{
+				LoadState (StateType.MainMenu);
+				_devModeSceneToLoad = "";
+			}
+		}
+
         public void LoadState(StateType currentState)
         {
             if (currentState == StateType.SplashScreen)
             {
+				Debug.Log ("Loading MainMenu");
                 SceneManager.LoadScene("MainMenu");
             }
             else if (currentState == StateType.MainMenu)
             {
+				Debug.Log ("Loading Level1");
                 SceneManager.LoadScene("Level1");
             }
             else
             {
-                Debug.Log("No load of that type supported.");
+                Debug.Log("No load of that type supported as of now.");
             }
         }
     }
